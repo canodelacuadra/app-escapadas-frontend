44434f01f3dbdece7a4d64ab818cf64a57c1f3b2
canodelacuadra <canodelacuadra@gmail.com>
2025-11-14 11:41:35 +0100

    pongo pie de imagen

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,14 +6,17 @@ const images = [
   {
     original: "https://picsum.photos/id/1018/1000/600/",
     thumbnail: "https://picsum.photos/id/1018/250/150/",
+    description:"Propuesta 1"
   },
   {
     original: "https://picsum.photos/id/1015/1000/600/",
     thumbnail: "https://picsum.photos/id/1015/250/150/",
+        description:"Propuesta  2"
   },
   {
     original: "https://picsum.photos/id/1019/1000/600/",
     thumbnail: "https://picsum.photos/id/1019/250/150/",
+        description:"Propuesta 3"
   },
 ];
 
@@ -29,6 +32,7 @@ export default function Gallery() {
       <ImageGallery
         items={images}
         autoPlay={true}
+        showBullets={true}
         thumbnailPosition="bottom"
         onClick={reDirigir}
       />
