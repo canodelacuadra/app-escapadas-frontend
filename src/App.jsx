40fae09dd0f390e7c43e5d4fe1e8b5913c5801a5
canodelacuadra <canodelacuadra@gmail.com>
2025-12-04 12:39:06 +0100

    formulario probado

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import Rutas from './pages/Rutas'
-import Creditos from './pages/Creditos'
+import Rutas from './pages/Rutas' // mapa
 import ListaRutas from './pages/ListaRutas'
 import { FaGithub } from "react-icons/fa";
 import Ruta from './pages/Ruta'
+import AgregarRuta from './pages/AgregarRuta'
 
 function App() {
 
@@ -20,12 +20,9 @@ function App() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/rutas">Rutas</Link></li>
              <li><Link to="/lista-rutas">Lista de Rutas</Link></li>
-        
+             <li><Link to="/agregar-ruta">Agregar Ruta</Link></li>
           </ul>
         </nav>
-        
-        
-        
         </header>
       <main>
         <Routes>
@@ -33,8 +30,7 @@ function App() {
           <Route path='/rutas' element={<Rutas/>}></Route>
           <Route path='/lista-rutas' element={<ListaRutas/>}></Route>
            <Route path="/ruta/:id" element={<Ruta />} />
-       
-
+            <Route path="/agregar-ruta" element={<AgregarRuta />} />
         </Routes>
       </main>
       <footer>
